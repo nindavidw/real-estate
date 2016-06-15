2d76fa00c3a5a23a3cbb13fd011989360ea77cd9
Sergey Korchevskiy <korchevskiysergey@hotmail.com>
2016-06-15 09:14:19 +0300

    feat: add search result page

--- SearchPage.js
+++ SearchPage.js
@@ -9,6 +9,9 @@ import {
   Image
 } from 'react-native'
 
+import SearchResults from './SearchResults'
+import houseImage from './Resources/house.png'
+
 const styles = StyleSheet.create({
   description: {
     marginBottom: 20,
@@ -53,11 +56,90 @@ const styles = StyleSheet.create({
     borderColor: '#48BBEC',
     borderRadius: 8,
     color: '#48BBEC'
+  },
+  image: {
+    width: 217,
+    height: 138
   }
 })
 
+const urlForQueryAndPage = (key, value, pageNumber) => {
+  const data = {
+    country: 'uk',
+    pretty: '1',
+    encoding: 'json',
+    listing_type: 'buy',
+    action: 'search_listings',
+    page: pageNumber
+  }
+  data[key] = value
+
+  const queryString = Object
+    .keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&')
+
+  return `http://api.nestoria.co.uk/api?${queryString}`
+}
+
 export default class SearchPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchString: 'london',
+      isLoading: false,
+      message: ''
+    }
+  }
+
+  _executeQuery(query) {
+    this.setState({
+      isLoading: true
+    })
+
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+        this.setState({
+          isLoading: false,
+          message: 'Something bad happened ' + error
+        }))
+  }
+
+  _handleResponse(response) {
+    this.setState({
+      isLoading: false,
+      message: ''
+    })
+    if(response.application_response_code.substr(0, 1) === '1') {
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {listings: response.listings}
+      })
+    } else {
+      this.setState({ message: 'Location not recognized: please try again. '})
+    }
+  }
+
+  onSearchPressed() {
+    const query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query)
+  }
+
+  onSearchTextChanged(event) {
+    this.setState({
+      searchString: event.nativeEvent.text
+    })
+  }
+
   render() {
+    const spinner = this.state.isLoading
+                  ? (<ActivityIndicatorIOS size="large" />)
+                  : (<View />)
+
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -69,14 +151,23 @@ export default class SearchPage extends Component {
         <View style={styles.flowRight}>
           <TextInput
             style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this.onSearchTextChanged.bind(this)}
             placeholder='Search via name or postcode'/>
-          <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor='#99d9f4'
+            onPress={this.onSearchPressed.bind(this)}
+          >
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
         <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
+        <Image source={houseImage} style={styles.image} />
+        {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     )
   }
---
+++ SearchResults.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Image,
+  View,
+  TouchableHighlight,
+  ListView,
+  Text
+} from 'react-native'
+
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+})
+
+export default class SearchResults extends Component {
+  constructor(props) {
+    super(props)
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.guid !== r2.guid
+    })
+
+    this.state = {
+      dataSource: dataSource.cloneWithRows(this.props.listings)
+    }
+  }
+
+  rowPressed(propertyGuid) {
+    const property = this.props
+      .listings
+      .find(prop => prop.guid === propertyGuid)
+  }
+
+  renderRow(rowData, sectionID, rowID) {
+    return (
+      <TouchableHighlight
+        onPress={() => this.rowPressed(rowData.guid)}
+        underlayColor = '#dddddd'>
+        <View>
+          <View style={styles.rowContainer}>
+          <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>${rowData.price}</Text>
+              <Text style={styles.title} numberofLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)} />
+    )
+  }
+}
